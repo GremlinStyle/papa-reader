44a167dad72cc8e5d890f81e903f659471ed9103
GremlinStyle <adamsperling535@gmail.com>
2026-08-18 15:51:27 +0200

    Switching aplay with pw-play cause errors

--- piper_run_me.py
+++ piper_run_me.py
@@ -3,7 +3,7 @@ import wave, subprocess, os
 import shutil
 
 def check_if_aplay_is_present():
-    if not shutil.which("aplay"):
+    if not shutil.which("pw-play"):
         print("ERROR")
         print("aplay is not installed")
         print("Arch Linux: sudo pacman -S alsa-utils")
@@ -22,5 +22,5 @@ def text_to_speech_language(language_onnx_file,text_to_speech,speed=1.5):
         with wave.open(test_file_name, "wb") as wav_file:
             voice.synthesize_wav(text_to_speech, wav_file,syn_config=syn_config)
 
-        subprocess.run(["aplay", test_file_name], check=True)
+        subprocess.run(["pw-play", test_file_name], check=True)
         os.remove(test_file_name)
